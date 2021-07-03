0331a8368a5016f2d16f58d617a205185a9b4cf0
durante.elena00 <durante.elena00@gmail.com>
2021-07-03 11:56:43 +0200

    completamento degli esercizi

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -1,3 +1,38 @@
 /* Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato. */
+
+//Initial variables
+var resultPlaceholder = document.getElementById("game-result-message");
+
+//Pick two random numbers (one for each player) between 1 and 6
+var player1Number = Math.round(Math.random() * 5) + 1;
+console.log("G1: " + player1Number);
+var player2Number = Math.round(Math.random() * 5) + 1;
+console.log("G2: " + player2Number);
+
+//Determine the result of the dice game (the bigger number wins)
+var winnerNumber;
+var loserNumber;
+var winner;
+var result;
+
+if (player1Number !== player2Number) {
+  if (player1Number > player2Number) {
+    winnerNumber = player1Number;
+    loserNumber = player2Number;
+    winner = "G1";
+  } else {
+    winnerNumber = player2Number;
+    loserNumber = player1Number;
+    winner = "G2";
+  }
+  var winningMessage = winner + " vince il gioco di dadi con " +  winnerNumber + " a " + loserNumber;
+  result = winningMessage
+} else {
+  var tieMessage = "Il gioco di dadi si conclude in pareggio con " + player1Number + " a " + player2Number;
+  result = tieMessage;
+}
+
+//Print an appropriate message
+resultPlaceholder.innerHTML = result;
--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -3,3 +3,49 @@
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
+
+//Initial variables
+var messagePlaceholder = document.getElementById("message-to-the-user");
+var welcomeMessage = "Benvenuto";
+var accessDeniedMessage =
+  "Spiacenti, l'indirizzo email inserito non è nella lista utenti autorizzati";
+var notValidEmailMessage = "L'indirizzo email inserito non è valido"
+
+//Define authorized users' list
+var authorizedEmailsList = [
+  "pippo@gmail.com",
+  "pluto@hotmail.com",
+  "paperino@outlook.it",
+];
+console.log(authorizedEmailsList);
+
+//Ask user his email
+var userEmail = prompt(
+  "Inserisci il tuo indirizzo email",
+  "pippo@gmail.com"
+).trim();
+
+//Verify if user is authorized
+var isUserAuthorized = false;
+var authorizedEmail;
+for (var i = 0; i < authorizedEmailsList.length; i++) {
+  authorizedEmail = authorizedEmailsList[i];
+
+  if (userEmail === authorizedEmail) {
+    isUserAuthorized = true;
+  }
+}
+
+// Validation
+//Print an appropriate message
+if (!userEmail) {
+  alert("Inserisci il tuo indirizzo email per accedere al sito");
+  messagePlaceholder.innerHTML = notValidEmailMessage;
+}
+else {
+  if (!isUserAuthorized) {
+    messagePlaceholder.innerHTML = accessDeniedMessage;
+  } else {
+    messagePlaceholder.innerHTML = welcomeMessage + " " + userEmail;
+  }
+}
